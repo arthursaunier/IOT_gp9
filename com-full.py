c3832a00b550e383a2b12416ef85f98b654b99d4
Arthur <arthur.saunier2@gmail.com>
2021-11-23 23:39:09 +0100

    com avec passerelle

---
+++ com-full.py
@@ -0,0 +1,24 @@
+from microbit import *
+import radio
+import reseau
+import uart
+
+radio.config(group=2)
+radio.on()
+reseau = reseau.RadioProtocol(2)
+uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=None, rx=None)
+
+while True:
+    message = reseau.receivePacket(radio.receive_bytes())
+    if message:
+        uart.write(message)
+        #display.scroll(message)
+    if uart.any():
+        content = uart.read()
+        if(content == 'TL'):
+            reseau.sendPacket("TL", 1)
+        else if(content == 'LT'):
+            reseau.sendPacket("LT", 1)
+    #if button_a.is_pressed():
+    #    reseau.sendPacket("Salut", 1)
+    sleep(1000)
