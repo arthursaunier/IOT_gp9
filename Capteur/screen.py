04b89d4aa47b3de42581aea0bcab36d94b0e72e6
Arthur <arthur.saunier2@gmail.com>
2021-12-02 17:23:58 +0000

    change structure of capteur

--- Capteur/screen.py
+++ Capteur/screen.py
@@ -15,13 +15,15 @@ reseau = reseau.RadioProtocol(1)
 while True:
     message = reseau.receivePacket(radio.receive_bytes())
     if message:
-        display.scroll(message)
-    if message == 'TL':
-        add_text(5,0,"T:" + str(temperature()))
-        add_text(5,3,"L:" + str(display.read_light_level()))
-    if message == 'LT':
-        add_text(5,0,"L:" + str(display.read_light_level()))
-        add_text(5,3,"T:" + str(temperature()))
-    if button_a.is_pressed():
-        reseau.sendPacket(str(temperature()) + ":"+ str(display.read_light_level()), 2)
-    sleep(1000)
+        #display.scroll(message)
+        if message == 'TL':
+            add_text(5,0,"T:" + str(temperature()))
+            add_text(5,3,"L:" + str(display.read_light_level()))
+        elif message == 'LT':
+            add_text(5,0,"L:" + str(display.read_light_level()))
+            add_text(5,3,"T:" + str(temperature()))
+    else:
+        add_text(2,2,"T:" + str(temperature()) + " L:"+ str(display.read_light_level()))
+    #if button_a.is_pressed():
+    reseau.sendPacket(str(temperature()) + ":"+ str(display.read_light_level()), 2)
+    sleep(2000)
