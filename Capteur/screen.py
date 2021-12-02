53a57558c608405cceecf96281e2262bef713446
Arthur <arthur.saunier2@gmail.com>
2021-12-02 18:20:05 +0000

    Fix send routine to not overcharge network

--- Capteur/screen.py
+++ Capteur/screen.py
@@ -12,18 +12,27 @@ radio.config(length=251, group=64)
 radio.on()
 reseau = reseau.RadioProtocol(1)
 
+Temp = 0
+Lum = 0
+oldTemp = 0
+oldLum = 0
+
 while True:
+    Temp = temperature()
+    Lum = display.read_light_level()
     message = reseau.receivePacket(radio.receive_bytes())
     if message:
         #display.scroll(message)
         if message == 'TL':
-            add_text(5,0,"T:" + str(temperature()))
-            add_text(5,3,"L:" + str(display.read_light_level()))
+            add_text(5,0,"T:" + str(Temp))
+            add_text(5,3,"L:" + str(Lum))
         elif message == 'LT':
-            add_text(5,0,"L:" + str(display.read_light_level()))
-            add_text(5,3,"T:" + str(temperature()))
+            add_text(5,0,"L:" + str(Lum))
+            add_text(5,3,"T:" + str(Temp))
     else:
         add_text(2,2,"T:" + str(temperature()) + " L:"+ str(display.read_light_level()))
-    #if button_a.is_pressed():
-    reseau.sendPacket(str(temperature()) + ":"+ str(display.read_light_level()), 2)
+    if (Temp != oldTemp or Lum != oldLum):
+        reseau.sendPacket(str(Temp) + ":"+ str(Lum), 2)
+        oldLum = Lum
+        oldTemp = Temp
     sleep(2000)
