fecf19012e15c5e918f7812f1ec7c9ef4bfe610d
Arthur <arthur.saunier2@gmail.com>
2021-12-03 16:54:06 +0000

    fix issue with writing in file

--- Gateway/data/file.py
+++ Gateway/data/file.py
@@ -3,13 +3,13 @@ from .log import Log, LogSystem
 class File(LogSystem):
     def __init__(self, filename):
         self.filename = f"{filename}.txt"
-        self.f = open(self.filename, "w+")
+        self.f = open(self.filename, "r+")
 
         self.last_log = None
 
     def save(self, log):
         self.f.write(f"{str(log.__dict__())}\n")
-        print(str(log.__dict__()))
+        #print(str(log.__dict__()))
         self.last_log = log
 
     def get_last_log(self):
