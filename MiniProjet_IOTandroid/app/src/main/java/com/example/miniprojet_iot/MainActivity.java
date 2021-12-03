2eeb926b5f9edd9e207193af6670486b23f8955c
HADJEB SAMI p1804898 <sami.hadjeb@etu.univ-lyon1.fr>
2021-12-03 22:00:45 +0100

    Ajout TimerTask

--- MiniProjet_IOTandroid/app/src/main/java/com/example/miniprojet_iot/MainActivity.java
+++ MiniProjet_IOTandroid/app/src/main/java/com/example/miniprojet_iot/MainActivity.java
@@ -18,6 +18,8 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -28,18 +30,17 @@ public class MainActivity extends AppCompatActivity {
     private InetAddress address;
     private DatagramSocket UDPSocket;
     private DatagramSocket UDPSocketRec;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
         EditText EText1 = findViewById(R.id.editTextTextPersonName);
         EditText EText2 = findViewById(R.id.editTextTextPersonName2);
-        TextView TView1 = findViewById(R.id.textView1);
-        TextView TView2 = findViewById(R.id.textView2);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
 
         try {
             UDPSocket = new DatagramSocket();
@@ -116,25 +117,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if ((String.valueOf(address) != "") || (String.valueOf(PORT) != "")){
-            byte[] buf = new byte[1024];
-            DatagramPacket dprec = new DatagramPacket(buf, 1024, address, PORT);
-            Executor executor = Executors.newSingleThreadExecutor();
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        UDPSocketRec.receive(dprec);
-                        String data = new String(dprec.getData(), 0, dprec.getLength());
-                        String[] valeurs = data.split(":");
-                        TView1.setText(valeurs[0] + "°C");
-                        TView2.setText(valeurs[1] + "UA");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
+        AskUpdate();
+
 
     }
+
+    private void AskUpdate() {
+        TextView TView1 = findViewById(R.id.textView1);
+        TextView TView2 = findViewById(R.id.textView2);
+        EditText EText1 = findViewById(R.id.editTextTextPersonName);
+        EditText EText2 = findViewById(R.id.editTextTextPersonName2);
+        timer = new Timer();
+
+        timer.scheduleAtFixedRate( new TimerTask() {
+            public void run() {
+
+                try{
+                    if (EText1.getText().toString().isEmpty()){
+                        IP = "127.0.0.1";
+                    }else{
+                        IP = EText1.getText().toString();
+                    }
+                    if (EText2.getText().toString().isEmpty()){
+                        PORT = 8080;
+                    }else{
+                        PORT = Integer.parseInt(EText2.getText().toString());
+                    }
+                    address = InetAddress.getByName(IP);
+
+                    String message = "update";
+                    DatagramPacket dp = new DatagramPacket(message.getBytes(StandardCharsets.UTF_8), message.length(),address, PORT);
+                    Executor executor = Executors.newSingleThreadExecutor();
+                    executor.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                UDPSocket.send(dp);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                    byte[] buf = new byte[1024];
+                    DatagramPacket dprec = new DatagramPacket(buf, 1024, address, PORT);
+                    Executor executorrec = Executors.newSingleThreadExecutor();
+                    executorrec.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                UDPSocketRec.receive(dprec);
+                                String data = new String(dprec.getData(), 0, dprec.getLength());
+                                String[] valeurs = data.split(":");
+                                TView1.setText(valeurs[0] + "°C");
+                                TView2.setText(valeurs[1] + "UA");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, 5000, 10000);
+    }
+
 }
