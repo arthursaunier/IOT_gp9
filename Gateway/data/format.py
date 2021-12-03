47ea40078405a0be794df54dd2829a8597015193
Arthur <arthur.saunier2@gmail.com>
2021-12-03 16:50:33 +0000

    function to reform string to send to app

--- Gateway/data/format.py
+++ Gateway/data/format.py
@@ -9,3 +9,8 @@ class Format:
         dico["temperature"] = parsed[0]
         dico["luminosity"] = parsed[1]
         return dico
+
+    @staticmethod
+    def reform(obj):
+        return str(str(obj["temperature"]) + ":" + str(obj["luminosity"]))
+       
