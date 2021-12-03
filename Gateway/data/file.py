dc1f8d957338f6bd72f413f44737d412d58cf79d
Arthur <arthur.saunier2@gmail.com>
2021-12-03 16:21:09 +0000

    Clean

--- Gateway/data/file.py
+++ Gateway/data/file.py
@@ -3,16 +3,16 @@ from .log import Log, LogSystem
 class File(LogSystem):
     def __init__(self, filename) -> None:
         self.filename = f"{filename}.txt"
-        self.f = open(self.filename, "r+")
+        self.f = open(self.filename, "w+")
 
         self.last_log = None
 
-    def save(self, log: Log):
+    def save(self, log):
         self.f.write(f"{str(log.__dict__())}\n")
 
         self.last_log = log
 
-    def get_last_log(self) -> Log:
+    def get_last_log(self):
         return self.last_log
 
     def close(self):
--- Gateway/data/log.py
+++ Gateway/data/log.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 
 class Log():
-    def __init__(self, temperature: float, luminosity: float, id_sensor: int, date = None):
+    def __init__(self, temperature, luminosity, date = None):
         self.temperature = temperature
         self.luminosity = luminosity
-        self.id_sensor = id_sensor
 
         if date != None: self.create_date = date
         else: self.create_date = datetime.now()
@@ -13,29 +12,28 @@ class Log():
         return {
             "temperature": self.temperature,
             "luminosity": self.luminosity,
-            "id_sensor": self.id_sensor,
             "create_date": str(self.create_date)
         }
     
     @staticmethod
-    def from_dict(obj: dict):
-        return Log(obj["temperature"], obj["luminosity"], obj["id_sensor"],obj["create_date"] if "create_date" in obj.keys() else None)
-
+    def from_dict(obj):
+        return Log(obj["temperature"], obj["luminosity"], obj["create_date"] if "create_date" in obj.keys() else None)
+        
     def __str__(self):
         return str(self.__dict__())
 
 class LogSystem():
-    def __init__(self, filename) -> None:
+    def __init__(self, filename):
         self.filename = filename
 
-    def save(self, log: Log):
+    def save(self, log):
         pass
 
-    def get_last_log(self) -> Log:
+    def get_last_log(self):
         pass
 
     def get_logs(self):
         pass
 
-    def close(self) -> None:
+    def close(self):
         pass
