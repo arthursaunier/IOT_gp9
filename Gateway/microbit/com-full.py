c19dc0544b978520a19750889d7930378091a5f9
Arthur <arthur.saunier2@gmail.com>
2021-12-03 13:24:03 +0000

    fix packet format

--- Gateway/microbit/com-full.py
+++ Gateway/microbit/com-full.py
@@ -7,11 +7,13 @@ radio.on()
 reseau = reseau.RadioProtocol(2)
 uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=None, rx=None)
 
+Packet_end = "[END]"
+
 while True:
     message = reseau.receivePacket(radio.receive_bytes())
     if message:
-        uart.write(message)
-        display.scroll(message)
+        uart.write(message + Packet_end)
+        #display.scroll(message)
     if uart.any():
         content = uart.read()
         if(content == 'TL'):
